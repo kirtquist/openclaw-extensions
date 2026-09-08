61df5496d88d9869ee8c7cc62d5958245bf905b8
kirtquist <kirtnystrom@hotmail.com>
2026-09-07 20:59:00 -0700

    fix(bible): run package build during install

--- extensions/bible-plugin/build_and_install.sh
+++ extensions/bible-plugin/build_and_install.sh
@@ -1,7 +1,6 @@
 npm install
-npm build
+npm run build
 
 ~/.nvm/versions/node/v24.14.1/bin/openclaw plugins install --force /home/kirt/projects/openclaw/extensions/extensions/bible-plugin
 
 openclaw gateway restart
-
